d7a1b03e3dc76c692d8b0986bb40d77929e522a5
Sebastian Vallejos <vallejos.sa@gmail.com>
2023-01-31 12:59:46 -0300

    mi primer commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//let boton= document.addEventListener("boton", myFunction);
+
+//function myFunction() {
+ // alert ("Hello World!");
+//}
+//quiero usar los eventlistener para que mi boton haga algo cuando le meten info
+
+
+//cómo usar el parseInt???
+//function roughScale(input) {
+  //  const parsed = parseInt("cardnumber");
+    //if (isNaN(parsed)) { return 0; }
+    //return parsed * 100;
